2fc34f0c30b4c560ea4067e7ba705cec7c3cd40b
SalaarShafi <shafisalaar@gmail.com>
2021-08-06 20:19:45 +0500

    made the senior users and wfm map functionality

--- src/senior-users.js
+++ src/senior-users.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
 const SeniorUsers = ({data, setData}) => {
+    const {users} = data;
     return (
         <>
-        
+        {
+                users.filter((item)=> item.role === 2).map((item, index)=> {
+                    const {role, firstName, lastName} = item;
+                    return (
+                <tr>
+            <th scope="row">{index}</th>
+            <td>{firstName}</td>
+            <td>{lastName}</td>
+            <td>Senior User</td>
+            <td>
+                <button type="button" className="btn btn-outline-danger btn-sm">Delete</button>
+            </td>
+        </tr>
+                    )
+                })
+            }
         </>
     )
 }
